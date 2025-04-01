e5deb64a1eae38be3d6119afb3713862f7e5d249
water <linshy76@mail2.sysu.edu.cn>
2025-04-01 22:45:27 +0800

    read env

--- database/index.go
+++ database/index.go
@@ -3,12 +3,16 @@ package database
 import (
 	"github.com/joho/godotenv"
 
+	"os"
+
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
+	if(os.Getenv("GO_ENV") != "docker") {
+		err := godotenv.Load()
+		if err != nil {
+			panic("Error loading .env file")
+		}
 	}
 
 	initMysql()
